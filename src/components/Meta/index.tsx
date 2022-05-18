a3e3573c9667af8acfe27ef0f5f7ed0adefa2894
Jan Rádl <radl.honza@gmail.com>
2022-05-18 09:46:47 +0200

    OG Image

--- src/components/Meta/index.tsx
+++ src/components/Meta/index.tsx
@@ -3,7 +3,6 @@ import Head from "next/head";
 export default function Meta({ title }: { title: string }) {
   return (
     <Head>
-      {" "}
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta charSet="utf-8" />
